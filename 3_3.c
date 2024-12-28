6607f410ebd753675d2fb0bb2fe48a65ed288ea9
VitorAlvess <53319601+VitorAlvess@users.noreply.github.com>
2024-12-28 11:04:12 -0300

    Primeiros Exercicios

---
+++ 3_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int valor1 = 0;
+    int valor2 = 0;
+    printf("Digite o primeiro valor: \n");
+    scanf("%d", &valor1);
+    printf("Digite o segundo valor: \n");
+    scanf("%d", &valor2);
+    printf("A soma é: %d", valor1 + valor2);
+}
